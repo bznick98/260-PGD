61a6f6a296366170ccdabb66f951b69e54eaaa50
Zongnan Bao <nick19981122@163.com>
2022-02-27 13:52:32 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# MMDET COLAB EXAMPLE
+# install dependencies: (use cu101 because colab has CUDA 10.1)
+!pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+# install mmcv-full thus we could use CUDA operators
+!pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
+
+# Install mmdetection
+!rm -rf mmdetection
+!git clone https://github.com/open-mmlab/mmdetection.git
+%cd mmdetection
+
+!pip install -e .
+
+# install Pillow 7.0.0 back in order to avoid bug in colab
+!pip install Pillow==7.0.0
