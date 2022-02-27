5dbfe97c434338360b532824f8263e527c93674b
Zongnan Bao <nick19981122@163.com>
2022-02-27 15:00:08 -0800

    unzip -o

--- colab_eval.sh
+++ colab_eval.sh
@@ -1,12 +1,12 @@
 # Run Eval Code
 cp /content/gdrive/MyDrive/data.zip /content/260-PGD
 cd /content/260-PGD
-unzip -y data
+unzip -o data
 cd /content/260-PGD/data
 # mkdir /content/260-PGD/data/mmdetection
 cp -r /content/260-PGD/mmdetection/ /content/260-PGD/data/
-unzip -y eval_code
-unzip -y images
+unzip -o eval_code
+unzip -o images
 mv images/* eval_code/select1000_new/
 mv /content/260-PGD/hnm_pgd.py /content/260-PGD/hnm_utils.py /content/260-PGD/data/eval_code/
 cd /content/260-PGD/data/eval_code
